bac47ca226176b678103095e2452d3166836fadb
Alexander Bologov <sano.strike14110@gmail.com>
2023-05-17 02:17:54 +0500

    setup db integration

--- ERP.TaskScheduler/Program.cs
+++ ERP.TaskScheduler/Program.cs
@@ -1,22 +1,30 @@
+using ERP.TaskScheduler.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services here
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors(builder => builder
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin());
+
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (app.Environment.IsProduction()) app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
