99428b9282a74a8996fa993fde5b7591d4a29ce4
Alexander Bologov <sano.strike14110@gmail.com>
2023-05-18 01:35:59 +0500

    consumer test

---
+++ ERP.TaskScheduler.Test.Consumer/Program.cs
@@ -0,0 +1,29 @@
+using ERP.TaskScheduler.Test.Consumer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<RabbitMqConsumer>();
+builder.Services.AddHostedService(provider => provider.GetRequiredService<RabbitMqConsumer>());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
